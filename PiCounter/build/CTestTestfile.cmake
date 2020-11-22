985b386b472a7f26a90686661747fdd75e61efc7
anisalopezruiz <anisalopezruiz@gmail.com>
2020-11-22 12:08:00 -0600

    My first checking

---
+++ PiCounter/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/PiCounter/src
+# Build directory: /home/ubuntu/PiCounter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("img_proc")
+subdirs("espros_tof_cam635")
